8c8b67adb55286c9d2d4d45edc9fadb7aeb2deee
sidens <sidens@gmail.com>
2017-07-04 12:20:19 -0700

    added NYC & capability for multi-city
    added multi-city capability, and new background style from Kate!

--- rater.js
+++ rater.js
@@ -1,43 +1,109 @@
 $(function() {
+//Create map of results to be returned from query
 var resultsmap = {};
-var ratings = ["http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/excellent_50.gif", "http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/good_50.gif", "http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/okay_50.gif", "http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/needstoimprove_50.gif"];
+//Create regions to pick from
+var regions = 
+{ 
+  Seattle:
+  [
+    "https://data.kingcounty.gov/resource/gkhn-e8mn.json", //url
+    "name", //dataset value for name
+    "business_id", //dataset value for business id
+    "grade", //dataset value for grade
+    "IS NOT NULL", //where params
+    "", //scale
+    {1:"http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/excellent_50.gif", 2:"http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/good_50.gif", 3:"http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/okay_50.gif", 4:"http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/needstoimprove_50.gif"}
+  ],
+  
+  NYC:
+  [
+    "https://data.cityofnewyork.us/resource/9w7m-hzhe.json", //url
+    "DBA", //dataset value for name
+    "CAMIS", //dataset value for business id
+    "GRADE", //dataset value for grade
+    "IS NOT NULL", //where params
+    "", //scale
+    {A:"img/NYC/a.png", B:"img/NYC/b.png", C:"img/NYC/c.png", P:"img/NYC/p.png"}
+  ]
+};
+//var ratings = ["http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/excellent_50.gif", "http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/good_50.gif", "http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/okay_50.gif", "http://www.kingcounty.gov/~/media/depts/health/environmental-health/images/food-safety/inspections/needstoimprove_50.gif"];
 
+// Excute this function when form is submitted
   $("#search").submit(function(event) {
+    //Prevent default submit, clear list in UI, and empty results map
     event.preventDefault();
-    $( "#rest_grades" ).empty();
     resultsmap = {};
+    $( "#rest_grades" ).empty();
     
+    //Find search parameters
     var search = $("#search .query").val();
+    var region = $("#search #city").val();
+    //console.log("region: "+region);
+
+    //Pick region for data based on user input
+    var userregion = regions[region];
+
+    // //Get dataset address
+    // var datasetaddress = userregion[0];
+
+    //Build query parameters
+    var selectquery = userregion[1]+", "+userregion[2]+", "+userregion[3];
+    console.log(selectquery);
+    var wherequery = userregion[3]+" "+userregion[4];
+    console.log(wherequery);
+
 
     $.ajax({
-    url: "https://data.kingcounty.gov/resource/gkhn-e8mn.json",
+    url: userregion[0],
     type: "GET",
     data: {
-      "$select" : "name, business_id, grade",
-      "$where": "grade IS NOT NULL", 
+      "$select" : selectquery ,
+      "$where": wherequery, 
       "$q": search,
       "$limit" : 50000,
       "$$app_token" : "CE7uCoAw5PG2KLRXRhRTCaIaM"
     }
     }).done(function(listings) {
+        //Set fieldnames for each dataset
+        var namefield = userregion[1];
+        var businessidfield = userregion[2];
+        var gradefield = userregion[3];
+        var ratings = userregion[6];
+
+        //console.log(namefield);  
+
         $.each(listings, function(idx, listing) {
-          // Fetch each resturaunt listing   
-          var name = listing.name;
-          var grade = listing.grade;
-          var business_id = listing.business_id;
-          // console.log(name);   
-          // console.log(grade);
-          resultsmap[business_id] = {name, grade};
+          // Fetch each resturaunt listing  
+          
+          //set universal params based on listing entries
+          // console.log(JSON.stringify(listing));  
+          var name = listing[namefield];
+          var grade = listing[gradefield];
+          var businessid = listing[businessidfield];
+          
+          console.log(name);
+
+          //add Businesses to the results map
+          resultsmap[businessid] = {name, grade};
+          //console.log(JSON.stringify(resultsmap[businessid]));  
+
         });
-        console.log("results map:");
-        console.log(resultsmap);
-        console.log(resultsmap.length);
+        // console.log("results map:");
+        // console.log(resultsmap);
+        // console.log(resultsmap.length);
 
         $.each(resultsmap, function(idx, result) {
-          console.log("map entry:");
-          console.log(result.name);
-          // console.log("index:" + idx);
-          $( "#rest_grades" ).append( "<span>Name: "+result.name+"</span>&nbsp;<span>Rating: <img src=\""+ratings[result.grade-1]+"\" /></span><br/>");
+           // console.log("map entry:");
+           // console.log(result.name);
+           // console.log("index:" + idx);
+
+        //results html, with ratings as key, converting to image
+          var resultgrade = result.grade;
+          console.log("Grade for result: "+resultgrade);
+          console.log("Img for result: "+ratings[resultgrade]);
+
+          $( "#rest_grades" ).append( "<span>Name: "+result.name+"</span>&nbsp;<span>Rating: <img width=50 src=\""+ratings[resultgrade]+"\" /></span><br/>");
+        //$( "#rest_grades" ).append( "<span>Name: "+result.name+"</span>&nbsp;<span>Rating: "+result.grade+"</span><br/>");
         });
     });
     // }).done(function(data) {
